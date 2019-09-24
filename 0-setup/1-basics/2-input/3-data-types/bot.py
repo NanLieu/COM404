4a8e50d61dc382077f5898b79c826f997f47af0d
NanLieu <4lieun20@solent.ac.uk>
2019-09-24 15:43:24 +0000

    Added code to demonstrate the use of different data types

---
+++ 0-setup/1-basics/2-input/3-data-types/bot.py
@@ -0,0 +1,10 @@
+# Ask user to enter their name
+print("What is your name human?")
+name = input()
+print("How old are you (in years)?")
+age = int(input())
+print("How tall are you (in meters)?")
+height = float(input())
+print("How much do you weigh (in kilograms)?")
+weight = int(input())
+print(name,"you are", age, "years old and your bmi is", (weight/height/height))
