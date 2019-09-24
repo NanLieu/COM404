3714df8b56472ddeb2e4de468d98af26d8f79176
NanLieu <4lieun20@solent.ac.uk>
2019-09-24 15:48:24 +0000

    Long string and new line testing

--- 0-setup/1-basics/2-input/3-data-types/bot.py
+++ 0-setup/1-basics/2-input/3-data-types/bot.py
@@ -8,3 +8,14 @@ height = float(input())
 print("How much do you weigh (in kilograms)?")
 weight = int(input())
 print(name,"you are", age, "years old and your bmi is", (weight/height/height))
+print()
+print()
+print("""Long string!!
+
+MORE SPACE!!
+
+MUAHAHAHAHA!!!""")
+print()
+print()
+print("Backslash new line! \nJump to new line!!")
+print()
