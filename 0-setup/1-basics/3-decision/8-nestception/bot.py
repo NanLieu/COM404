c0ba33b9d7e53070f4f3a589d43fdeb91d18baad
NanLieu <4lieun20@solent.ac.uk>
2019-10-01 14:34:11 +0000

    Added code to demonstrate the use of a nested decision statement.

---
+++ 0-setup/1-basics/3-decision/8-nestception/bot.py
@@ -0,0 +1,26 @@
+print("Where should I look?")
+where_to_look = input()
+if (where_to_look == "in the bedroom"):
+    print("Where in the bedroom should I look?")
+    in_bedroom = input()
+    if (in_bedroom == "under the bed"):
+        print("Found some shoes but no battery")
+    else:
+        print("Found some mess but no battery")
+elif (where_to_look == "in the bathroom"):
+    print("Where in the bathroom should I look?")
+    in_bathroom = input()
+    if (in_bathroom == "in the bathtub"):
+        print("Found a rubber duck but no battery")
+    else:
+        print("It is wet but I found no battery")
+elif (where_to_look == "in the lab"):
+    print("Where in the lab should I look?")
+    in_lab = input()
+    if (in_lab == "on the table"):
+        print("Yes! I found my battery!")
+    else:
+        print("Found some tools but no battery")
+else:
+    print("I do not know where that is but I will keep looking.")
+
