56408b1e61177ba17fff939b7e3f3c2731a81560
NanLieu <4lieun20@solent.ac.uk>
2019-10-08 14:31:43 +0000

    Added code to demonstrate the use of a while loop in performing calculations with user input.

---
+++ 0-setup/1-basics/4-repetition/1-while-loop/5-sum-100/bot.py
@@ -0,0 +1,10 @@
+# Display message
+print("Calculation the sum of the first 100 numbers...")
+final_number = 0
+counter = 0
+while(counter < 100):
+    counter = counter + 1
+    final_number = final_number + counter
+
+
+print("Done! The answer is", final_number)
---
+++ 0-setup/1-basics/4-repetition/1-while-loop/6-sum-user-numbers/bot.py
@@ -0,0 +1,15 @@
+print("How many numbers should I sum up?")
+numbers_to_sum = int(input())
+number = 0
+total = 0
+print()
+
+while (numbers_to_sum > number):
+    number = number + 1
+    print("Please enter number", number, "of ", numbers_to_sum, ":")
+    input_numbers = int(input())
+    total = input_numbers + total
+
+print()
+print("The answer is",total,".")
+
