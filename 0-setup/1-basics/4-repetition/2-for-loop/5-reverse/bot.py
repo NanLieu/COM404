c779a3627c0eb7977aa2bce7aa0d9a02090b7c81
NanLieu <4lieun20@solent.ac.uk>
2019-10-08 16:19:18 +0000

    added comments

--- 0-setup/1-basics/4-repetition/2-for-loop/5-reverse/bot.py
+++ 0-setup/1-basics/4-repetition/2-for-loop/5-reverse/bot.py
@@ -1,8 +1,14 @@
+# Question
 print("What phrase do you see?")
+# User Input
 phrase = input()
+# Displaying message
 print("\nReversing...\n")
+# Display message whilst intending to add extra test at the end
 print("\nThe phrase is: ", end="")
+#For loop of position in phrase variable,
 for position in range(len(phrase) - 1, -1 , -1):
+    # print phrase based on position
     print(phrase[position], end="")
 print()
 
