e23c7bc681e5b3bc7471a9fa49883cc245ff9339
NanLieu <4lieun20@solent.ac.uk>
2019-10-15 13:05:18 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ 0-setup/1-basics/5-functions/2-nesting/bot.py
@@ -0,0 +1,9 @@
+def whats_there():
+    print("What lies before us?")
+    what_is_there = input()
+    if (what_is_there == "a large boulder"):
+        print("It's time to run!")
+    else:
+        print("We will be fine")
+
+whats_there()
