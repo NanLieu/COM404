e763c96899d39874368b8398608df626b20f92f9
NanLieu <4lieun20@solent.ac.uk>
2019-10-15 13:50:19 +0000

    Added code to demonstrate the use of a user-defined function with a loop.

--- 0-setup/1-basics/5-functions/3-parameter/bot.py
+++ 0-setup/1-basics/5-functions/3-parameter/bot.py
@@ -1,3 +1,4 @@
+# Defined a function with the parameter "plan" with if function built in
 def escape_by(plan):
     if (plan == "jumping over"):
         print("We cannot escape that way! The boulder is too big!")
@@ -6,7 +7,7 @@ def escape_by(plan):
     elif (plan =="going deeper"):
         print("That might just work! Let's go deeper!")
 
-
+# Calls function with a defined parameter
 escape_by("jumping over")
 escape_by("running around")
 escape_by("going deeper")
