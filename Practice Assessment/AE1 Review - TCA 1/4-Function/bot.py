097c2233a9e67e5c35aca77182bed31d5c40e4c0
NanLieu <4lieun20@solent.ac.uk>
2019-10-22 13:44:35 +0000

    Onto second part of the assessment!

--- Practice Assessment/AE1 Review - TCA 1/4-Function/bot.py
+++ Practice Assessment/AE1 Review - TCA 1/4-Function/bot.py
@@ -1,13 +1,15 @@
 # Created function to determine what code to run dependent on the parameter
 def item_from_suitcase (item):
+    # Prints first statement for user
+    print("I wonder what is in my suitcase...")
+    # Decision made based on parameter
     if (item == "toothbrush"):
         print("A toothbrush. Well, got to have clean teeth!\n")
     elif (item == "spidey suit"):
         print("My Spidey suit! I won't be needing this. I am on holiday.\n")
     else:
         print("An unexpected item! It could be useful.\n")
-# Prints first statement for user
-print("I wonder what is in my suitcase...\n")
+
 # Calls function 3 times with 3 different parameters
 item_from_suitcase("toothbrush")
 item_from_suitcase("belt")
---
+++ Practice Assessment/AE1 Review - TCA 2/1-Input-and-Output/bot.py
@@ -0,0 +1,6 @@
+# Bella asks the Beast a question
+print("What happens when the last petal falls?")
+# Requests the Beast's answer with "I will remain a Beast forever"
+beast_answer = input()
+# Prints out the Beast's answer
+print("My dear Bella when the last petal falls",beast_answer)
