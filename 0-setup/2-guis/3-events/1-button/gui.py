7307140db160849de70e6a24aaee708ade651088
Nan Lieu <4lieun20@solent.ac.uk>
2019-11-19 14:50:39 +0000

    Added code to display a gui using the grid layout manager.

--- 0-setup/2-guis/3-events/1-button/gui.py
+++ 0-setup/2-guis/3-events/1-button/gui.py
@@ -9,19 +9,45 @@ class Gui(Tk):
         self.title("Tickets")
         
         # add components
+        self.__add_outer_frame()
         self.__add_heading_label()
         self.__add_instruction_label()
         self.__add_tickets_entry()
         self.__add_buy_button()
+
+    
+    def __add_outer_frame(self):
+        self.outer_frame = Frame()
+        self.outer_frame.grid(row=0, column=0)
+        self.outer_frame.configure( bg="#eee", 
+                                    padx=10, 
+                                    pady=10)
         
     def __add_heading_label(self):
-        pass
+        self.heading_label = Label(self.outer_frame)
+        self.heading_label.grid(row=0, column=0, columnspan=2)
+        self.heading_label.configure(   bg="#eee",
+                                        font="Arial 18",
+                                        text="Entrance Ticket")
+        
         
     def __add_instruction_label(self):
-        pass
+        self.instruction_label = Label(self.outer_frame)
+        self.instruction_label.grid(row=1, column=0, columnspan=2, sticky=W+E)
+        self.instruction_label.configure(   bg="#eee",
+                                            font="Arial 10",
+                                            text="How many tickets are needed?",
+                                            padx=10,
+                                            pady=10)
         
     def __add_tickets_entry(self):
-        pass
+        self.email_entry = Entry(self.outer_frame)
+        self.email_entry.grid(row=2, column=1, sticky=W)
+        self.email_entry.configure(width=40)
         
     def __add_buy_button(self):
-        pass
+        self.buy_button = Button(self.outer_frame)
+        self.buy_button.grid(row=3, column=0, columnspan=2, pady=10)
+        self.buy_button.configure(      bg="#eee",
+                                        text="Buy",
+                                        padx=20)
